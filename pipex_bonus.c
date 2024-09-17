f579c5964fcd3b498ae7822ed641a1109b0740bc
Rui Pires <rpires-c@c2r5s4.42porto.com>
2024-09-17 11:53:38 +0100

    pipex bonus done?

--- pipex_bonus.c
+++ pipex_bonus.c
@@ -5,24 +5,111 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: rpires-c <rpires-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/09 11:26:18 by rpires-c          #+#    #+#             */
-/*   Updated: 2024/09/11 15:29:32 by rpires-c         ###   ########.fr       */
+/*   Created: 2024/09/12 15:31:46 by rpires-c          #+#    #+#             */
+/*   Updated: 2024/09/17 11:52:57 by rpires-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
+
+t_btree *create_node(char *cmd)
+{
+    t_btree *new_node = malloc(sizeof(t_btree));
+    if (!new_node)
+        return NULL;
+    new_node->cmd = cmd;
+    new_node->left = NULL;
+    new_node->right = NULL;
+    return new_node;
+}
+
+t_btree *build_tree(char **argv, int start, int end)
+{
+    int	mid;
+	t_btree	*root;
+	
+	if (start > end)
+        return NULL;
+    mid = (start + end) / 2;
+    root = create_node(argv[mid]);
+    root->left = build_tree(argv, start, mid - 1);
+    root->right = build_tree(argv, mid + 1, end);
+    return root;
+}
+
+void process_tree(char **argv, t_btree *node, char **envp, int *is_first_command)
+{
+    int fd[2];
+    pid_t pid;
+    int filein;
+    int fileout;
+
+    if (node == NULL)
+        return;
+    if (pipe(fd) == -1)
+        pipe_error();
+    pid = fork();
+    if (pid == -1)
+        fork_error();
+    if (pid == 0) 
+    {
+        if (node->left == NULL && node->right == NULL) 
+        {
+            if (*is_first_command)
+            {
+                filein = open_file(argv[1], 2); 
+                if (filein == -1)
+                    open_file_error();
+                dup2(filein, STDIN_FILENO); 
+                close(filein);              
+                *is_first_command = 0;    
+            }
+            dup2(fd[1], STDOUT_FILENO);
+            close(fd[0]);
+            execute(node->cmd, envp); 
+        }
+        else 
+        {
+            close(fd[0]);              
+            dup2(fd[1], STDOUT_FILENO);
+            if (node->left)
+                process_tree(argv, node->left, envp, is_first_command);
+            if (node->right)
+                process_tree(argv, node->right, envp, is_first_command);
+        }
+    }
+    else 
+    {
+        while (*argv)
+            argv++;
+
+        fileout = open_file(*(argv - 1), 1);  
+        if (fileout == -1)
+            open_file_error();
+        dup2(fileout, STDOUT_FILENO);
+        close(fileout);
+        close(fd[1]);            
+        dup2(fd[0], STDIN_FILENO);
+        close(fd[0]);
+        waitpid(pid, NULL, 0);
+    }
+}
 
 void	here_doc(char *limiter, int argc)
 {
 	pid_t	reader;
 	int		fd[2];
 	char	*line;
-
+    
 	if (argc < 6)
 		usage();
 	if (pipe(fd) == -1)
 		pipe_error();
 	reader = fork();
+    if (reader == -1)
+    {
+        fork_error();
+    }
 	if (reader == 0)
 	{
 		close(fd[0]);
@@ -41,55 +128,25 @@ void	here_doc(char *limiter, int argc)
 	}
 }
 
-void	child_process(char *argv, char **envp)
+int main(int argc, char **argv, char **envp)
 {
-	pid_t	pid;
-	int		fd[2];
+    t_btree *root;
+    int first_cmd_flag;
 
-	if (pipe(fd) == -1)
-		pipe_error();
-	pid = fork();
-	if (pid == -1)
-		fork_error();
-	if (pid == 0)
-	{
-		close(fd[0]);
-		dup2(fd[1], STDOUT_FILENO);
-		execute(argv, envp);
-	}
-	else
-	{
-		close(fd[1]);
-		dup2(fd[0], STDIN_FILENO);
-		waitpid(pid, NULL, 0);
-	}
-}
-
-int	main(int argc, char **argv, char **envp)
-{
-	int	i;
-	int	filein;
-	int	fileout;
-
-	if (argc >= 5)
-	{
-		if (ft_strncmp(argv[1], "here_doc", 8) == 0)
+    first_cmd_flag = 1;
+    if (argc >= 5)
+    {
+        /* if (ft_strncmp(argv[1], "here_doc", 8) == 0)
 		{
-			i = 3;
 			fileout = open_file(argv[argc - 1], 0);
 			here_doc(argv[2], argc);
 		}
-		else
-		{
-			i = 2;
-			fileout = open_file(argv[argc - 1], 1);
-			filein = open_file(argv[1], 2);
-			dup2(filein, STDIN_FILENO);
-		}
-		while (i < argc - 2)
-			child_process(argv[i++], envp);
-		dup2(fileout, STDOUT_FILENO);
-		execute(argv[argc - 2], envp);
-	}
-	usage();
+        else */
+        {
+            root = build_tree(argv, 2, argc - 2);
+            process_tree(argv, root, envp, &first_cmd_flag);
+            execute(argv[argc - 2], envp);
+        }
+    }
+    usage();
 }
